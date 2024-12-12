f8313e629bd4783bbc4b1ff8eb5948cb10f489f0
Khaled Ahmed <kmhsn217@gmail.com>
2024-12-12 01:45:07 -0800

    edited frontend

--- frontend/src/components/fixtures.jsx
+++ frontend/src/components/fixtures.jsx
@@ -1,51 +1,55 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import './Fixtures.css'
 
 function Fixtures() {
   const [fixtures, setFixtures] = useState([])
   const [loading, setLoading] = useState(true)
 
-  const teamLogos = {
-    Arsenal: 'https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg',
-    'Aston Villa':
-      'https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Aston_Villa_FC_new_crest.svg/300px-Aston_Villa_FC_new_crest.svg.png',
-    Brentford:
-      'https://upload.wikimedia.org/wikipedia/en/2/2a/Brentford_FC_crest.svg',
-    Brighton:
-      'https://upload.wikimedia.org/wikipedia/en/f/fd/Brighton_%26_Hove_Albion_logo.svg',
-    Chelsea: 'https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg',
-    'Crystal Palace':
-      'https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/Crystal_Palace_FC_logo_%282022%29.svg/350px-Crystal_Palace_FC_logo_%282022%29.svg.png',
-    Everton:
-      'https://upload.wikimedia.org/wikipedia/en/7/7c/Everton_FC_logo.svg',
-    Liverpool:
-      'https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg',
-    'Man City':
-      'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/380px-Manchester_City_FC_badge.svg.png',
-    'Man Utd':
-      'https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/400px-Manchester_United_FC_crest.svg.png',
-    Newcastle:
-      'https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Newcastle_United_Logo.svg/400px-Newcastle_United_Logo.svg.png',
-    Southampton:
-      'https://upload.wikimedia.org/wikipedia/en/c/c9/FC_Southampton.svg',
-    'Tottenham Hotspur':
-      'https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg',
-    'West Ham':
-      'https://upload.wikimedia.org/wikipedia/en/c/c2/West_Ham_United_FC_logo.svg',
-    Wolves:
-      'https://upload.wikimedia.org/wikipedia/en/f/fc/Wolverhampton_Wanderers.svg',
-    "Nott'm Forest":
-      'https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/Nottingham_Forest_F.C._logo.svg/220px-Nottingham_Forest_F.C._logo.svg.png',
-    Leicester:
-      'https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Leicester_City_crest.svg/380px-Leicester_City_crest.svg.png',
-    Fulham:
-      'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Fulham_FC_%28shield%29.svg/300px-Fulham_FC_%28shield%29.svg.png',
-    Bournemouth:
-      'https://upload.wikimedia.org/wikipedia/en/e/e5/AFC_Bournemouth_%282013%29.svg',
-    Watford: 'https://upload.wikimedia.org/wikipedia/en/e/e2/Watford.svg',
-    Ipswich:
-      'https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Ipswich_Town.svg/312px-Ipswich_Town.svg.png',
-  }
+  // UseMemo to avoid recreating teamLogos on every render
+  const teamLogos = useMemo(
+    () => ({
+      Arsenal: 'https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg',
+      AstonVilla:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Aston_Villa_FC_new_crest.svg/300px-Aston_Villa_FC_new_crest.svg.png',
+      Brentford:
+        'https://upload.wikimedia.org/wikipedia/en/2/2a/Brentford_FC_crest.svg',
+      Brighton:
+        'https://upload.wikimedia.org/wikipedia/en/f/fd/Brighton_%26_Hove_Albion_logo.svg',
+      Chelsea: 'https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg',
+      CrystalPalace:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/Crystal_Palace_FC_logo_%282022%29.svg/350px-Crystal_Palace_FC_logo_%282022%29.svg.png',
+      Everton:
+        'https://upload.wikimedia.org/wikipedia/en/7/7c/Everton_FC_logo.svg',
+      Liverpool:
+        'https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg',
+      ManCity:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/380px-Manchester_City_FC_badge.svg.png',
+      ManUtd:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/400px-Manchester_United_FC_crest.svg.png',
+      Newcastle:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Newcastle_United_Logo.svg/400px-Newcastle_United_Logo.svg.png',
+      Southampton:
+        'https://upload.wikimedia.org/wikipedia/en/c/c9/FC_Southampton.svg',
+      TottenhamHotspur:
+        'https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg',
+      WestHam:
+        'https://upload.wikimedia.org/wikipedia/en/c/c2/West_Ham_United_FC_logo.svg',
+      Wolves:
+        'https://upload.wikimedia.org/wikipedia/en/f/fc/Wolverhampton_Wanderers.svg',
+      NottmForest:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/Nottingham_Forest_F.C._logo.svg/220px-Nottingham_Forest_F.C._logo.svg.png',
+      Leicester:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Leicester_City_crest.svg/380px-Leicester_City_crest.svg.png',
+      Fulham:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Fulham_FC_%28shield%29.svg/300px-Fulham_FC_%28shield%29.svg.png',
+      Bournemouth:
+        'https://upload.wikimedia.org/wikipedia/en/e/e5/AFC_Bournemouth_%282013%29.svg',
+      Watford: 'https://upload.wikimedia.org/wikipedia/en/e/e2/Watford.svg',
+      Ipswich:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Ipswich_Town.svg/312px-Ipswich_Town.svg.png',
+    }),
+    []
+  )
 
   useEffect(() => {
     const fetchData = async () => {
@@ -86,7 +90,7 @@ function Fixtures() {
     }
 
     fetchData()
-  }, [])
+  }, [teamLogos]) // Include teamLogos as a dependency
 
   // Group fixtures by game week
   const groupedFixtures = fixtures.reduce((acc, fixture) => {
