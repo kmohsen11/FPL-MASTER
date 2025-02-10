2702d85a321c858b1aea595a9eae9e430d3e2630
Khaled Ahmed <kmhsn217@gmail.com>
2025-02-10 11:03:37 -0800

    Bind to dynamic port on Render

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,11 @@
+import os
+from backend.app import create_app
 
-from app import create_app
+# Get the port from the environment variable, default to 5000 if not set
+port = int(os.environ.get("PORT", 5000))
+
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    # Run the app on the dynamically assigned port
+    app.run(debug=True, host="0.0.0.0", port=port)
