e9bf21db1ee07dd70a5956b9d3fd407e23e6a083
Khaled Ahmed <kmhsn217@gmail.com>
2025-02-10 14:10:31 -0800

    Fixed Procfile and app entry point

--- frontend/src/components/Navbar_page.jsx
+++ frontend/src/components/Navbar_page.jsx
@@ -1,14 +1,11 @@
 import { Link } from 'react-router-dom'
 import "./Navbar_style.css";
+import logo from "../assets/FPL-Master_Logo.png"; // Adjust the path based on where Navbar_page.jsx is
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img
-        src="./src/assets/FPL-Master_Logo.png"
-        alt="FPL-Master Logo"
-        className="navbar-logo"
-      />
+      <img src={logo} alt="FPL-Master Logo" className="navbar-logo" />
       <div className="navbar-links">
         <Link to="/">Home</Link>
         <Link to="/fixtures">Fixtures</Link>
@@ -17,7 +14,7 @@ const Navbar = () => {
         <Link to="/contact">Contact</Link>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
