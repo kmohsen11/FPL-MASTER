a9c1e04e9653ea9397d1fc8a3dd6661c7064e857
Khaled Ahmed <kmhsn217@gmail.com>
2025-02-11 21:41:27 -0800

    Fix redundant database initialization

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, current_app
 #import db from its bath
 from . import db
 from .models import Player  # Corrected import
