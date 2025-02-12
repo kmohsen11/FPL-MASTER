0810b9013fcd8f89029a3db699cbd20e231a78bd
Khaled Ahmed <kmhsn217@gmail.com>
2025-02-11 21:16:00 -0800

    Fix redundant database initialization

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
-from app import db
+#import db from its bath
+from . import db
 from app.models import Player  # Corrected import
 from sqlalchemy.orm import joinedload
 from sqlalchemy.exc import IntegrityError
