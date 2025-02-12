beb6abeb7afde734e0ecb74e86f428f34a688581
Khaled Ahmed <kmhsn217@gmail.com>
2025-02-11 21:57:11 -0800

    Update to use PostgreSQL

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,39 +1,25 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate  # Flask-Migrate for handling migrations
-import os
+from flask_migrate import Migrate
 
-# Initialize the database instance globally
 db = SQLAlchemy()
-
-# Initialize Flask-Migrate
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Z9Q7E2KzC6b1f6z2qz9g3mE7P4q8zN0l')
 
-    # Set up configuration from environment variables or defaults
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///instance/example.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoid unnecessary overhead
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Z9Q7E2KzC6b1f6z2qz9g3mE7P4q8zN0l')  # To handle sessions and CSRF
-
-    # Allow CORS for all routes
     CORS(app)
-
-    # Initialize the database with the app
     db.init_app(app)
-
-    # Initialize Flask-Migrate with the app and database
     migrate.init_app(app, db)
 
-   
-
-    # Ensure all tables are created
     with app.app_context():
-        # Register Blueprints for routes
-        from .routes import api  # Make sure this import is correct
+        from .routes import api
         app.register_blueprint(api, url_prefix='/api')
-        db.create_all()  # This creates tables if they don't already exist.
+        db.create_all()
 
     return app
