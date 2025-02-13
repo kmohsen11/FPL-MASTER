580ece0193b5d19ebc8be72806514929653eaac3
Khaled Ahmed <kmhsn217@gmail.com>
2025-02-13 01:00:32 -0800

    Switch to using MY_DATABASE_URL for Neon DB

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,7 +9,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql://uaf90ij1pjpf4r:pcfa7822d3a6c41c89db5ee4e725ad6bdefe9294e2096b4598069b3ae4f0b03fd@cbdhrtd93854d5.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/ddkt0vbqsn6bgq')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql://neondb_owner:npg_GpF01WdNIYBM@ep-frosty-dream-a5ya4h7n-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Z9Q7E2KzC6b1f6z2qz9g3mE7P4q8zN0l')
 
